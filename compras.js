5aa5c3c7799dd942be0a0ce0c9dd9d95ad99b884
vamp-ctrl <victoraugustomauriciopereira@gmail.com>
2025-09-02 10:23:48 -0400

    02/09

---
+++ compras.js
@@ -0,0 +1,1 @@
+const lista = document.querySelectorAll('li');
---
+++ script.js
@@ -0,0 +1,17 @@
+// const paragrafo = window.document.getElementById('paragrafo');
+// const lista = document.querySelectorAll('li');
+// const link = document.querySelector('a');
+
+// console.log(paragrafo.textContent);
+// paragrafo.textContent = 'Texto alterado via JavaScript!';
+// console.log(lista.length);
+// console.log(link.getAttribute('href'));
+
+// link.setAttribute('href', 'https://www.ifro.edu.br');
+// link.setAttribute('target', '_blank');
+// paragrafo.style.backgroundColor = 'red';
+
+// addEventListener('click', (ev)=>{
+//     alert('Clicou em algum lugar da página!');d
+//     ev.target.style.backgroundColor = 'blue';
+// });
