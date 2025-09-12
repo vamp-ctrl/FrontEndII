ee90ccec388d88948ec7741fc3a550ede59ff5e8
vamp-ctrl <victoraugustomauriciopereira@gmail.com>
2025-09-12 11:41:13 -0400

    12/09

---
+++ primeiroprojeto/src/app/Produtos/page.js
@@ -0,0 +1,10 @@
+import styles from "./produtos.module.css";
+
+export default function Produtos() {
+  return (
+    <div>
+      <h1>Produtos</h1>
+      <p>Lista de produtos</p>
+    </div>
+  );
+}
